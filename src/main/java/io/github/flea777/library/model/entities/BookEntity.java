e94bd5a597f9575375f2536b79c23b55c3bc32b3
Pedro Bim <pedrodiniz2@outlook.com>
2025-04-08 20:25:27 -0300

    Book entity created

--- src/main/java/io/github/flea777/library/model/entities/BookEntity.java
+++ src/main/java/io/github/flea777/library/model/entities/BookEntity.java
@@ -1,5 +1,62 @@
 package io.github.flea777.library.model.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class BookEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String author;
+  private String publisher;
+  private Boolean isAvailable;
+
+  public BookEntity(String _name, String _author, String _publisher) {
+    name = _name;
+    author = _author;
+    publisher = _publisher;
+    isAvailable = true;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public Boolean getIsAvailable() {
+    return isAvailable;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof BookEntity)) return false;
+    BookEntity that = (BookEntity) o;
+    return Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
