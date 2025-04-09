28abe80721d58bd0801ffc4f0967c3f8bbff3a23
Pedro Bim <pedrodinizz2@outlook.com>
2025-04-09 00:17:36 -0300

    update imports

--- src/main/java/io/github/flea777/library/api/BookController.java
+++ src/main/java/io/github/flea777/library/api/BookController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-
 
 @RestController
 @RequestMapping("/books")
@@ -38,7 +36,7 @@ public class BookController {
   public Book insert(@RequestBody Book data) {
       return service.insert(data);
   }
-  
+
   @DeleteMapping
   public void delete(@PathVariable Long id) {
     service.delete(id);
