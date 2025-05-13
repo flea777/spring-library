005c330b9128c3e99a2a6e264ea2e8618c0397cb
yuricsg <yuriicardo004@gmail.com>
2025-05-13 13:30:20 -0300

    more unity tests

--- src/test/java/io/github/flea777/library/LibraryApplicationTests.java
+++ src/test/java/io/github/flea777/library/LibraryApplicationTests.java
@@ -3,11 +3,23 @@ package io.github.flea777.library;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 @SpringBootTest
 class LibraryApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void stringShouldNotBeEmpty() {
+		String testString = "Hello, Library!";
+		assertFalse(testString.isEmpty(), "The string should not be empty");
+	}
+
+	@Test
+	void listShouldContainElement() {
+		java.util.List<String> list = java.util.Arrays.asList("Book", "Author", "Publisher");
+		assertTrue(list.contains("Author"), "The list should contain 'Author'");
 	}
 
 }
